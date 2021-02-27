16f8cbd55507ad549090842532f8761fa3845058
Mark Strendin <mark.strendin@gmail.com>
2021-02-26 19:38:42 -0600

    Add bus garage environment sensor to dashboard 1

--- wwwroot/dashboards/it-1/initialize.js
+++ wwwroot/dashboards/it-1/initialize.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     weather_init(WEATHER_UPDATE_URL);
     time_init(DATE_AND_TIME_URL);
     envmon_init("environment-DO-ss", "Sensor-001", "https://dashboard-api.lskysd.ca/environmentmonitor/10.177.54.161/");
+    envmon_init("environment-DO-bg", "Sensor-001", "https://dashboard-api.lskysd.ca/environmentmonitor/10.177.54.181/");
     envmon_update(); // Call this after all are initialized
     
     itwatchdog_init("itwatchdog-DO-SWITCHES","https://dashboard-api.lskysd.ca/ITWatchDog/10.177.54.129/9F0004A3F25158C3", true);
