7c356cd11194ee55d19c9f056f7a39e0f3d3aeff
Mark Strendin <mark.strendin@lskysd.ca>
2021-07-28 11:17:38 -0600

    Adjust box positions

--- wwwroot/dashboards/it-1/initialize.js
+++ wwwroot/dashboards/it-1/initialize.js
@@ -15,8 +15,8 @@ $(document).ready(function(){
 
     // Initialize school tiles (mostly testing right now)
     datatile_init("school_info_box_container", "OFFICE",1570,475, [5], 6, [6,7]);    
-    datatile_init("school_info_box_container", "NBCHS",1500,0, [27], 25, [21,22,23,24]);   
-    datatile_init("school_info_box_container", "BCS",1340,700, [9], 8, [1]); 
+    datatile_init("school_info_box_container", "NBCHS",1540,25, [27], 25, [21,22,23,24]);   
+    datatile_init("school_info_box_container", "BCS",1350,700, [9], 8, [1]); 
     datatile_init("school_info_box_container", "Bready",1140,145, [10], 9, [2]);
     datatile_init("school_info_box_container", "Cando",585,648, [11], 10, [33,34]);   
     datatile_init("school_info_box_container", "CKCS",262,370, [13], 12, [4,5]);
@@ -27,7 +27,7 @@ $(document).ready(function(){
     datatile_init("school_info_box_container", "Kerrobert",348,818, [18], 17, [10]);
     datatile_init("school_info_box_container", "Lawrence",1330,100, [3], 3, [11]);
     datatile_init("school_info_box_container", "Leoville",630,60, [19], 18, [12]);
-    datatile_init("school_info_box_container", "Luseland",100,660, [20], 19, [13]);
+    datatile_init("school_info_box_container", "Luseland",50,660, [20], 19, [13]);
     datatile_init("school_info_box_container", "Macklin",16,522, [21], 20, [14,15]);
     datatile_init("school_info_box_container", "Maymont",715,530, [23], 21, [32]);
     datatile_init("school_info_box_container", "McKitrick",1330,205, [24], 22, [16]);
@@ -36,8 +36,8 @@ $(document).ready(function(){
     datatile_init("school_info_box_container", "NCES",478,502, [28], 26, [20]);
     datatile_init("school_info_box_container", "SHS",775,140, [30], 28, [25,26]);
     datatile_init("school_info_box_container", "St. Vital",1150,700, [33], 31, [27,28]);
-    datatile_init("school_info_box_container", "UCHS",285,508, [31], 29, [29,30]);
-    datatile_init("school_info_box_container", "UPS", 285, 633, [32], 30, [31]);
+    datatile_init("school_info_box_container", "UCHS",265,508, [31], 29, [29,30]);
+    datatile_init("school_info_box_container", "UPS", 265, 633, [32], 30, [31]);
     datatile_update();
 
 });
