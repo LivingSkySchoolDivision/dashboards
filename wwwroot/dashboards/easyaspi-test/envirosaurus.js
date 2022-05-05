21bb6850cd9f82d121eaf1c4b9adbe0783dbbb0f
Mark Strendin <mark.strendin@lskysd.ca>
2022-05-05 16:20:17 -0600

    Refactor EAP dashboard a bit to support multiple maps. Add connaught map.

--- wwwroot/dashboards/easyaspi-test/envirosaurus.js
+++ wwwroot/dashboards/easyaspi-test/envirosaurus.js
@@ -1,49 +1,78 @@
-function eap_init_tile(containerid, xpos, ypos, sensorserial, pinlocation)
+const low_temp_value = 18;
+const high_temp_value = 27;
+const stale_error_message = "COMMUNICATIONS FAILURE";
+const consider_stale_after_minutes = 25;
+
+var visible_map = "";
+
+function eap_init_weather_box(containerid,weathercode) 
 {
-    var html = "";
+    var weatherhtml = "";
+    weatherhtml +='<div id="weather_conditions">';
+    weatherhtml +='    <div class="weather_title">Local conditions</div>';
+    weatherhtml +='    <div id="weather-container">';
+    weatherhtml +='        <div id="weather-detail-icon"><img src="../../img/weather/default.png"></div>';
+    weatherhtml +='        <div>';
+    weatherhtml +='            <div id="weather-temperature">###&deg;C</div>';
+    weatherhtml +='            <div id="weather-feelslike">Feels like ###&deg;C</div>';
+    weatherhtml +='            <div id="weather-extra"> ';
+    weatherhtml +='                Humid: <div id="weather-humidity"></div><br/>';
+    weatherhtml +='                Wind: <div id="weather-wind"></div>';
+    weatherhtml +='            </div>';
+    weatherhtml +='        </div>';
+    weatherhtml +='    </div>';
+    weatherhtml +='</div>';
+    $('#' + containerid).append(weatherhtml);
+}
 
-    html += '<div class="eap_sensor" id="eap_sensor_' + sensorserial + '" style="top: ' + ypos + 'px; left: ' + xpos + 'px;">';
+function eap_init_map(mapcontainerid, mapcodename, mapname, mapweathercode) 
+{
+    var maphtml = "";    
+    maphtml +='<div class="eap_map_screen">';    
+    maphtml +='    <div class="eap_map_title">' + mapname + '</div>';      
+    maphtml +='    <div class="eap_map_container">';
+    maphtml += '     <img class="eap_map" src="maps/' + mapcodename + '.png">';
+    maphtml +='      <div class="eap_pin_container" id="eap_pin_container_' + mapcodename + '"></div>';
+    maphtml += '   </div>';    
+    maphtml +='    <div class="eap_tile_container" id="eap_sensor_container_'+mapcodename+'"></div>';
+    maphtml +='</div>';
+    $('#' + mapcontainerid).append(maphtml);
+}
 
-    if (pinlocation.toLowerCase() == "bottomleft") {
-        html += '    <div class="eap_pin eap_pin_bottom_left"></div>';
-    }
+function eap_init_sensor(mapcodename, xpos, ypos, sensorserial)
+{
+    var pinhtml = "";    
+    pinhtml += '<div class="eap_pin eap_pin_loading" id="eap_pin_' + sensorserial + '" style="top: ' + ypos + 'px; left: ' + xpos + 'px;">000</div>';
+    
+    var pincontainerid = "eap_pin_container_" + mapcodename;
+    $('#' + pincontainerid).append(pinhtml);
 
-    if (pinlocation.toLowerCase() == "bottomright") {
-        html += '    <div class="eap_pin eap_pin_bottom_right"></div>';
-    }
+    var tilehtml = "";
+    tilehtml += '<div class="eap_sensor" id="eap_sensor_' + sensorserial + '" >';
+    tilehtml += '    <div class="eap_sensor_name" id="eap_' + sensorserial + '_name">---</div>';
+    tilehtml += '    <div class="eap_sensor_tile_error hidden" id="eap_sensor_tile_error_' + sensorserial + '"></div>';
+    tilehtml += '    <div class="eap_sensor_sensors" id="eap_sensor_tile_value_area_' + sensorserial + '">';
+    tilehtml += '        <div class="eap_sensor_data_field">';
+    tilehtml += '            <div class="eap_sensor_data_field_icon"><img src="../../img/thermometer-half.svg"></div>';
+    tilehtml += '            <div class="eap_sensor_data_field_value eap_sensor_temp"><div class="eap_data_value" id="eap_' + sensorserial + '_temp">--</div>&deg;C</div>';
+    tilehtml += '        </div>';
+    tilehtml += '        <div class="eap_sensor_data_field">';
+    tilehtml += '            <div class="eap_sensor_data_field_icon"><img src="../../img/humidity.svg"></div>';
+    tilehtml += '            <div class="eap_sensor_data_field_value eap_sensor_humid"><div class="eap_data_value" id="eap_' + sensorserial + '_humid">--</div>%</div>';
+    tilehtml += '        </div>';
+    tilehtml += '        <div class="eap_sensor_data_field">';
+    tilehtml += '            <div class="eap_sensor_data_field_icon"><img src="../../img/barometer.svg"></div>';
+    tilehtml += '            <div class="eap_sensor_data_field_value eap_sensor_pres"><div class="eap_data_value" id="eap_' + sensorserial + '_pressure">--</div> kPa</div>';
+    tilehtml += '        </div>';
+    tilehtml += '        <div class="eap_sensor_data_field">';
+    tilehtml += '            <div class="eap_sensor_data_field_icon"><img src="../../img/light.svg"></div>';
+    tilehtml += '            <div class="eap_sensor_data_field_value eap_sensor_light"><div class="eap_data_value" id="eap_' + sensorserial + '_lux">--</div> lux</div>';
+    tilehtml += '        </div>';
+    tilehtml += '    </div>';
+    tilehtml += '</div>';
 
-    if (pinlocation.toLowerCase() == "topleft") {
-        html += '    <div class="eap_pin eap_pin_top_left"></div>';
-    }
-
-    if (pinlocation.toLowerCase() == "topright") {
-        html += '    <div class="eap_pin eap_pin_top_right"></div>';
-    }
-
-    html += '    <div class="eap_sensor_name" id="eap_' + sensorserial + '_name">---</div>';
-    html += '    <div class="eap_sensor_sensors">';
-    html += '        <div class="eap_sensor_data_field">';
-    html += '            <div class="eap_sensor_data_field_icon"><img src="../../img/thermometer-half.svg"></div>';
-    html += '            <div class="eap_sensor_data_field_value eap_sensor_temp"><div class="eap_data_value" id="eap_' + sensorserial + '_temp">--</div>&deg;C</div>';
-    html += '        </div>';
-    html += '        <div class="eap_sensor_data_field">';
-    html += '            <div class="eap_sensor_data_field_icon"><img src="../../img/humidity.svg"></div>';
-    html += '            <div class="eap_sensor_data_field_value eap_sensor_humid"><div class="eap_data_value" id="eap_' + sensorserial + '_humid">--</div>%</div>';
-    html += '        </div>';
-    html += '        <div class="eap_sensor_data_field">';
-    html += '            <div class="eap_sensor_data_field_icon"><img src="../../img/barometer.svg"></div>';
-    html += '            <div class="eap_sensor_data_field_value eap_sensor_pres"><div class="eap_data_value" id="eap_' + sensorserial + '_pressure">--</div> kPa</div>';
-    html += '        </div>';
-    html += '        <div class="eap_sensor_data_field">';
-    html += '            <div class="eap_sensor_data_field_icon"><img src="../../img/light.svg"></div>';
-    html += '            <div class="eap_sensor_data_field_value eap_sensor_light"><div class="eap_data_value" id="eap_' + sensorserial + '_lux">--</div> lux</div>';
-    html += '        </div>';
-    html += '    </div>';
-    html += '</div>';
-
-    $('#' + containerid).append(html);
-
-    console.log("Initializing EAP sensor " + sensorserial + " into div " + containerid + " at position " + xpos + "," + ypos);
+    var tilecontainerid = "eap_sensor_container_" + mapcodename;
+    $('#' + tilecontainerid).append(tilehtml);
 }
 
 function eap_update_names()
@@ -53,7 +82,8 @@ function eap_update_names()
         // For each sensor reading returned, try to update a corresponding div
 
         $.each(data, function(sensors, sensor) {
-            $("#eap_" + sensor.deviceSerialNumber + "_name").html(sensor.deviceFriendlyName);
+            $("#eap_" + sensor.deviceSerialNumber + "_name").html(('000' + sensor.assignedNumber).slice(-3));
+            $("#eap_pin_" + sensor.deviceSerialNumber).html(('000' + sensor.assignedNumber).slice(-3));
         });
     });
 }
@@ -73,44 +103,67 @@ function eap_update_data()
             var reading_timestamp = Date.parse(reading.timestampUTC);
             var minutes_since_update = Math.round((utc_timestamp - reading_timestamp) / 1000 / 60);
             
+            var sensorHasIssue = false;
 
-            if (minutes_since_update > 15) {                
-                console.log("Stale sensor: " + reading.deviceSerialNumber);
-                $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_sensor_good");
+            if (minutes_since_update > consider_stale_after_minutes) {     
+                $("#eap_" + reading.deviceSerialNumber + "_temp").html("???");
+                
+                $("#eap_sensor_tile_error_" + reading.deviceSerialNumber).html(stale_error_message);
+                $("#eap_sensor_tile_value_area_" + reading.deviceSerialNumber + "").addClass("hidden");
+                $("#eap_sensor_tile_error_" + reading.deviceSerialNumber + "").removeClass("hidden");
+
                 $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_stale_sensor");
                 $("#eap_" + reading.deviceSerialNumber + "_temp").html("???");
                 $("#eap_" + reading.deviceSerialNumber + "_humid").html("???");
                 $("#eap_" + reading.deviceSerialNumber + "_pressure").html("???");
                 $("#eap_" + reading.deviceSerialNumber + "_lux").html("???");
+                $("#eap_pin_" + reading.deviceSerialNumber + "").addClass("eap_pin_stale"); 
+                sensorHasIssue = true;
                 return true;
             } else {
+                $("#eap_sensor_tile_error_" + reading.deviceSerialNumber).html("");
+                $("#eap_sensor_tile_value_area_" + reading.deviceSerialNumber + "").removeClass("hidden");
+                $("#eap_sensor_tile_error_" + reading.deviceSerialNumber + "").addClass("hidden");
+                
                 $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_stale_sensor");
-                $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_sensor_good");
+                $("#eap_pin_" + reading.deviceSerialNumber + "").removeClass("eap_pin_stale");            
             }
 
             // If the data is over 10 minutes old, display warning
-
             $("#eap_" + reading.deviceSerialNumber + "_temp").html(tempValue);
             $("#eap_" + reading.deviceSerialNumber + "_humid").html(Math.round(reading.humidityPercent * 10) / 10);
             $("#eap_" + reading.deviceSerialNumber + "_pressure").html(Math.round(reading.pressure));
             $("#eap_" + reading.deviceSerialNumber + "_lux").html(Math.round(reading.lux));
 
-
-
             // Temperature alarms
-            if (tempValue > 26) {
-                $("#eap_" + reading.deviceSerialNumber + "_temp").addClass("eap_temp_too_high");
+            if (tempValue >= high_temp_value) {
+                $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_temp_too_high");
+                $("#eap_pin_" + reading.deviceSerialNumber).addClass("eap_pin_temp_too_high");
+                sensorHasIssue = true;
             } else {
-                $("#eap_" + reading.deviceSerialNumber + "_temp").removeClass("eap_temp_too_high");
+                $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_temp_too_high");
+                $("#eap_pin_" + reading.deviceSerialNumber).removeClass("eap_pin_temp_too_high");
             }
 
-            if (tempValue < 17) {
-                $("#eap_" + reading.deviceSerialNumber + "_temp").addClass("eap_temp_too_low");
+            if (tempValue <= low_temp_value) {
+                $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_temp_too_low");
+                $("#eap_pin_" + reading.deviceSerialNumber).addClass("eap_pin_temp_too_low");
+                sensorHasIssue = true;
             } else {
-                $("#eap_" + reading.deviceSerialNumber + "_temp").removeClass("eap_temp_too_low");
+                $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_temp_too_low");
+                $("#eap_pin_" + reading.deviceSerialNumber).removeClass("eap_pin_temp_too_low");
             }
 
+            // Removing the "loading" styles
+            $("#eap_pin_" + reading.deviceSerialNumber).removeClass("eap_pin_loading");
 
+
+            // If there are no issues, show the "good" pin styles
+            if (sensorHasIssue) {
+                $("#eap_pin_" + reading.deviceSerialNumber + "").removeClass("eap_pin_good");                 
+            } else {
+                $("#eap_pin_" + reading.deviceSerialNumber + "").addClass("eap_pin_good");                  
+            }
 
 
         });
--- wwwroot/dashboards/easyaspi-test/initialize.js
+++ wwwroot/dashboards/easyaspi-test/initialize.js
@@ -4,25 +4,45 @@
 $(document).ready(function(){
     weather_init(WEATHER_UPDATE_URL);
 
-    // 188 wide
-    // 85 tall
-    eap_init_tile("eap_sensor_container", 1125, 30, "00000000623713ca", "bottomleft"); // 001 - IT Warehouse 1
-    eap_init_tile("eap_sensor_container", 1020, 380, "0000000008775767", "topright"); // 034 - Bus garage hall
-    eap_init_tile("eap_sensor_container", 1100, 245, "00000000ca8bced2", "bottomleft"); // 002 - Kobelsky
-    eap_init_tile("eap_sensor_container", 720, 180, "00000000e75f9b1c", "bottomleft"); // 003 - Board room
-    eap_init_tile("eap_sensor_container", 890, 40, "000000002200107e", "bottomright"); // 004 - IT fridge
-    eap_init_tile("eap_sensor_container", 1125, 120, "00000000f3e4c91a", "bottomleft"); // 146 - IT ceiling
-    eap_init_tile("eap_sensor_container", 870, 290, "0000000090b57373", "topright"); // 019 - IT Office 1
-    eap_init_tile("eap_sensor_container", 1380, 430, "000000007b463ac5", "topleft"); // 008 - fitness
-    eap_init_tile("eap_sensor_container", 1280, 330, "00000000b7deddad", "bottomleft"); // 095 - Busg break room    
-    eap_init_tile("eap_sensor_container", 600, 60, "00000000ad7a9938", "bottomright"); // 013 - server room
-    eap_init_tile("eap_sensor_container", 500, 250, "00000000bffd768e", "topleft"); // 006 - Staff
-    eap_init_tile("eap_sensor_container", 480, 150, "00000000d3d673a2", "none"); // 005 - accounting label room
-    eap_init_tile("eap_sensor_container", 190, 230, "000000003e2e3911", "topright"); // 148 - SS hall
-    eap_init_tile("eap_sensor_container", 115, 100, "0000000036553dc2", "bottomleft"); // 025 - SS back hall
-    eap_init_tile("eap_sensor_container", 1625, 460, "00000000f62d67b8", "bottomright"); // 143 - Bus Garage SE wall
+    // Central Office
+    eap_init_map("map_container", "do", "Central Services", "sk-34");
+    eap_init_sensor("do", 1105, 110, "00000000623713ca"); // 001 - IT Warehouse 1
+    eap_init_sensor("do", 1090, 335, "00000000ca8bced2"); // 002 - Kobelsky
+    eap_init_sensor("do", 700, 230, "00000000e75f9b1c"); // 003 - Board room
+    eap_init_sensor("do", 860, 170, "000000002200107e"); // 004 - IT fridge
+    eap_init_sensor("do", 545, 190, "00000000d3d673a2"); // 005 - accounting label room
+    eap_init_sensor("do", 500, 220, "00000000bffd768e"); // 006 - Board room
+    eap_init_sensor("do", 1380, 410, "000000007b463ac5"); // 008 - fitness
+    eap_init_sensor("do", 800, 120, "00000000ad7a9938"); // 013 - server room
+    eap_init_sensor("do", 1070, 290, "0000000090b57373"); // 019 - IT Office 1
+    eap_init_sensor("do", 105, 170, "0000000036553dc2"); // 025 - SS back hall
+    eap_init_sensor("do", 1200, 370, "0000000008775767"); // 034 - Bus garage hall    
+    eap_init_sensor("do", 1240, 405, "00000000b7deddad"); // 095 - Busg break room    
+    eap_init_sensor("do", 1820, 470, "00000000f62d67b8"); // 143 - Bus Garage SE wall
+    eap_init_sensor("do", 1105, 170, "00000000f3e4c91a"); // 146 - IT ceiling
+    eap_init_sensor("do", 395, 210, "000000003e2e3911"); // 148 - SS hall
 
-    eap_init_tile("eap_sensor_container", 50, 430, "00000000bd4533ee", "none"); // 057 - Purposefully unplugged stale sensor
+    // Connaught
+    eap_init_map("map_container", "connaught", "Connaught Elementary", "sk-34");
+    eap_init_sensor("connaught", 1098, 45, "00000000e60fdab9"); // 018 - Connaught - Room 13
+    eap_init_sensor("connaught", 905, 250, "0000000092342574"); // 037 - Connaught - Room 07
+    eap_init_sensor("connaught", 650, 380, "0000000072f3eb5a"); // 039 - Connaught - Office
+    eap_init_sensor("connaught", 1336, 45, "00000000bdc00ac2"); // 055 - Connaught - Room 15
+    eap_init_sensor("connaught", 650, 485, "000000002f9812c1"); // 058 - Connaught - Room 43
+    eap_init_sensor("connaught", 1388,435, "00000000b64e54f7"); // 081 - Connaught - Gym
+    eap_init_sensor("connaught", 905, 640, "000000008f7967fb"); // 090 - Connaught - Staff room
+
+    eap_init_sensor("connaught", 700, 320, "000000003a0dbe59"); // 096 - Connaught - Speced (room 05)
+
+    eap_init_sensor("connaught", 805, 250, "000000001a88fabe"); // 100 - Connaught - Room 06
+    eap_init_sensor("connaught", 805, 600, "0000000090eb8a96"); // 104 - Connaught - Room 40
+    eap_init_sensor("connaught", 1098, 245, "00000000c95114ca"); // 107 - Connaught - Room 09
+    eap_init_sensor("connaught", 1288, 215, "000000003dad6417"); // 110 - Connaught - Room 17    
+    eap_init_sensor("connaught", 830, 440, "00000000d5fda0ee"); // 134 - Connaught - Library
+    eap_init_sensor("connaught", 1000, 250, "000000008151a13f"); // 139 - Connaught - Room 08
+    eap_init_sensor("connaught", 1214, 45, "0000000070e41fe4"); // 145 - Connaught - Room 14
+
+
 
     eap_update_names();
     eap_update_data();
