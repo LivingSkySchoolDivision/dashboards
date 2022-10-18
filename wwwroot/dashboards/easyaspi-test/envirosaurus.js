c9214661dbb2104e1988aa4544ccd28d3cc39db3
Mark Strendin <mark.strendin@lskysd.ca>
2022-10-18 14:51:50 -0600

    Easy as Pi demo dashboard fixes Fix issues where API endpoint was broken causing the whole dashboard to fail

--- wwwroot/dashboards/easyaspi-test/envirosaurus.js
+++ wwwroot/dashboards/easyaspi-test/envirosaurus.js
@@ -106,9 +106,8 @@ function eap_init_sensor(mapcodename, xpos, ypos, sensorserial)
     $('#' + pincontainerid).append(pinhtml);
 
     var tilehtml = "";
-    tilehtml += '<div class="eap_sensor" id="eap_sensor_' + sensorserial + '" >';
-    tilehtml += '    <div class="eap_sensor_name" id="eap_' + sensorserial + '_name">---</div>';
-    tilehtml += '    <div class="eap_sensor_tile_error hidden" id="eap_sensor_tile_error_' + sensorserial + '"></div>';
+    tilehtml += '<div class="eap_sensor" style="position:relative" id="eap_sensor_' + sensorserial + '" >';
+    tilehtml += '    <div class="eap_sensor_name" id="eap_' + sensorserial + '_name">---</div>';        
     tilehtml += '    <div class="eap_sensor_sensors" id="eap_sensor_tile_value_area_' + sensorserial + '">';
     tilehtml += '        <div class="eap_sensor_data_field">';
     tilehtml += '            <div class="eap_sensor_data_field_icon"><img src="../../img/thermometer-half.svg"></div>';
@@ -126,7 +125,8 @@ function eap_init_sensor(mapcodename, xpos, ypos, sensorserial)
     tilehtml += '            <div class="eap_sensor_data_field_icon"><img src="../../img/light.svg"></div>';
     tilehtml += '            <div class="eap_sensor_data_field_value eap_sensor_light"><div class="eap_data_value" id="eap_' + sensorserial + '_lux">--</div> lux</div>';
     tilehtml += '        </div>';
-    tilehtml += '    </div>';
+    tilehtml += '    </div>';    
+    tilehtml += '    <div class="eap_sensor_tile_error hidden" style="font-size: 12pt; position: absolute; bottom: 0; left: 0; width: 100%;" id="eap_' + sensorserial + '_error"></div>';
     tilehtml += '</div>';
 
     var tilecontainerid = "eap_sensor_container_" + mapcodename;
@@ -148,85 +148,121 @@ function eap_update_names()
     });
 }
 
+function minutes_since_last_polled(last_poll_value)
+{
+    var now = new Date();
+    var parsed_date = new Date(Date.parse(last_poll_value));
+    var minutes_since = Math.round((now - parsed_date) / 1000 / 60);
+    return minutes_since;
+}
+
 function eap_update_data()
 {
     console.log("Updating EAP readings from " + EAP_API_PATH);
-    $.getJSON(EAP_API_PATH + "/SensorReading", function(data) {
+    $.getJSON(EAP_API_PATH + "/Sensors", function(data) {
         // For each sensor reading returned, try to update a corresponding div
 
         var local_timestamp = new Date();
         var utc_timestamp = Date.UTC(local_timestamp.getUTCFullYear(),local_timestamp.getUTCMonth(), local_timestamp.getUTCDate(), local_timestamp.getUTCHours(), local_timestamp.getUTCMinutes(), local_timestamp.getUTCSeconds(), local_timestamp.getUTCMilliseconds());
 
-        $.each(data, function(readings, reading) {
-            var tempValue = Math.round(reading.temperatureCelsius * 10) / 10
+        $.each(data, function(sensors, sensor) 
+        {
+            // Check to see if the last data is actually recent
+            // If the data is null or stale, apply styles to indicate error on the tile and pin
+            var minutes_since_last_poll = minutes_since_last_polled(sensor.lastSensorReadingUTC); 
+            console.log(sensor.id + ": " + minutes_since_last_poll + " min");         
 
-            var reading_timestamp = Date.parse(reading.timestampUTC);
-            var minutes_since_update = Math.round((utc_timestamp - reading_timestamp) / 1000 / 60);
+            if ((null == sensor.lastSensorReading) || (minutes_since_last_poll > consider_stale_after_minutes))
+            { 
+                // Show comms error
+                $("#eap_" + sensor.deviceSerialNumber + "_error").removeClass("hidden");
+                $("#eap_" + sensor.deviceSerialNumber + "_error").html("Last seen<br>" + sensor.lastHeartbeatUTC);
+                console.log("Comms error: " + sensor.id);                
 
-            var sensorHasIssue = false;
+                // Show error styles
+                $("#eap_sensor_" + sensor.deviceSerialNumber + "").addClass("eap_stale_sensor");
+                $("#eap_pin_" + sensor.deviceSerialNumber + "").addClass("eap_pin_stale");
 
-            if (minutes_since_update > consider_stale_after_minutes) {
-                $("#eap_" + reading.deviceSerialNumber + "_temp").html("???");
 
-                $("#eap_sensor_tile_error_" + reading.deviceSerialNumber).html("Last seen: " + minutes_since_update + " minutes ago");
-                $("#eap_sensor_tile_value_area_" + reading.deviceSerialNumber + "").addClass("hidden");
-                $("#eap_sensor_tile_error_" + reading.deviceSerialNumber + "").removeClass("hidden");
-
-                $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_stale_sensor");
-                $("#eap_" + reading.deviceSerialNumber + "_temp").html("???");
-                $("#eap_" + reading.deviceSerialNumber + "_humid").html("???");
-                $("#eap_" + reading.deviceSerialNumber + "_pressure").html("???");
-                $("#eap_" + reading.deviceSerialNumber + "_lux").html("???");
-                $("#eap_pin_" + reading.deviceSerialNumber + "").addClass("eap_pin_stale");
-                sensorHasIssue = true;
-                return true;
             } else {
-                $("#eap_sensor_tile_error_" + reading.deviceSerialNumber).html("");
-                $("#eap_sensor_tile_value_area_" + reading.deviceSerialNumber + "").removeClass("hidden");
-                $("#eap_sensor_tile_error_" + reading.deviceSerialNumber + "").addClass("hidden");
+                var reading = sensor.lastSensorReading;
 
-                $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_stale_sensor");
-                $("#eap_pin_" + reading.deviceSerialNumber + "").removeClass("eap_pin_stale");
+                // Hide comms error
+                $("#eap_" + reading.deviceSerialNumber + "_error").addClass("hidden");
+
+                // Populate data
+
+                // Show warnings based on available data (hot/cold/etc)
+
+                var reading = sensor.lastSensorReading;
+                var tempValue = Math.round(reading.temperatureCelsius * 10) / 10
+                var reading_timestamp = Date.parse(reading.timestampUTC);
+                var minutes_since_update = Math.round((utc_timestamp - reading_timestamp) / 1000 / 60);
+                var sensorHasIssue = false;
+
+                if (minutes_since_update > consider_stale_after_minutes) {
+                    $("#eap_" + reading.deviceSerialNumber + "_temp").html("???");
+
+                    $("#eap_sensor_tile_error_" + reading.deviceSerialNumber).html("Last seen: " + minutes_since_update + " minutes ago");
+                    $("#eap_sensor_tile_value_area_" + reading.deviceSerialNumber + "").addClass("hidden");
+                    $("#eap_sensor_tile_error_" + reading.deviceSerialNumber + "").removeClass("hidden");
+
+                    $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_stale_sensor");
+                    $("#eap_" + reading.deviceSerialNumber + "_temp").html("???");
+                    $("#eap_" + reading.deviceSerialNumber + "_humid").html("???");
+                    $("#eap_" + reading.deviceSerialNumber + "_pressure").html("???");
+                    $("#eap_" + reading.deviceSerialNumber + "_lux").html("???");
+                    $("#eap_pin_" + reading.deviceSerialNumber + "").addClass("eap_pin_stale");
+                    sensorHasIssue = true;
+                    return true;
+                } else {
+                    $("#eap_sensor_tile_error_" + reading.deviceSerialNumber).html("");
+                    $("#eap_sensor_tile_value_area_" + reading.deviceSerialNumber + "").removeClass("hidden");
+                    $("#eap_sensor_tile_error_" + reading.deviceSerialNumber + "").addClass("hidden");
+
+                    $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_stale_sensor");
+                    $("#eap_pin_" + reading.deviceSerialNumber + "").removeClass("eap_pin_stale");
+                }
+
+                // If the data is over 10 minutes old, display warning
+                $("#eap_" + reading.deviceSerialNumber + "_temp").html(tempValue);
+                $("#eap_" + reading.deviceSerialNumber + "_humid").html(Math.round(reading.humidityPercent * 10) / 10);
+                $("#eap_" + reading.deviceSerialNumber + "_pressure").html(Math.round(reading.pressure));
+                $("#eap_" + reading.deviceSerialNumber + "_lux").html(Math.round(reading.lux));
+
+                // Temperature alarms
+                if (tempValue >= high_temp_value) {
+                    $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_temp_too_high");
+                    $("#eap_pin_" + reading.deviceSerialNumber).addClass("eap_pin_temp_too_high");
+                    sensorHasIssue = true;
+                } else {
+                    $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_temp_too_high");
+                    $("#eap_pin_" + reading.deviceSerialNumber).removeClass("eap_pin_temp_too_high");
+                }
+
+                if (tempValue <= low_temp_value) {
+                    $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_temp_too_low");
+                    $("#eap_pin_" + reading.deviceSerialNumber).addClass("eap_pin_temp_too_low");
+                    sensorHasIssue = true;
+                } else {
+                    $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_temp_too_low");
+                    $("#eap_pin_" + reading.deviceSerialNumber).removeClass("eap_pin_temp_too_low");
+                }
+
+                // Removing the "loading" styles
+                $("#eap_pin_" + reading.deviceSerialNumber).removeClass("eap_pin_loading");
+
+
+                // If there are no issues, show the "good" pin styles
+                if (sensorHasIssue) {
+                    $("#eap_pin_" + reading.deviceSerialNumber + "").removeClass("eap_pin_good");
+                } else {
+                    $("#eap_pin_" + reading.deviceSerialNumber + "").addClass("eap_pin_good");
+                }
+
             }
-
-            // If the data is over 10 minutes old, display warning
-            $("#eap_" + reading.deviceSerialNumber + "_temp").html(tempValue);
-            $("#eap_" + reading.deviceSerialNumber + "_humid").html(Math.round(reading.humidityPercent * 10) / 10);
-            $("#eap_" + reading.deviceSerialNumber + "_pressure").html(Math.round(reading.pressure));
-            $("#eap_" + reading.deviceSerialNumber + "_lux").html(Math.round(reading.lux));
-
-            // Temperature alarms
-            if (tempValue >= high_temp_value) {
-                $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_temp_too_high");
-                $("#eap_pin_" + reading.deviceSerialNumber).addClass("eap_pin_temp_too_high");
-                sensorHasIssue = true;
-            } else {
-                $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_temp_too_high");
-                $("#eap_pin_" + reading.deviceSerialNumber).removeClass("eap_pin_temp_too_high");
-            }
-
-            if (tempValue <= low_temp_value) {
-                $("#eap_sensor_" + reading.deviceSerialNumber + "").addClass("eap_temp_too_low");
-                $("#eap_pin_" + reading.deviceSerialNumber).addClass("eap_pin_temp_too_low");
-                sensorHasIssue = true;
-            } else {
-                $("#eap_sensor_" + reading.deviceSerialNumber + "").removeClass("eap_temp_too_low");
-                $("#eap_pin_" + reading.deviceSerialNumber).removeClass("eap_pin_temp_too_low");
-            }
-
-            // Removing the "loading" styles
-            $("#eap_pin_" + reading.deviceSerialNumber).removeClass("eap_pin_loading");
-
-
-            // If there are no issues, show the "good" pin styles
-            if (sensorHasIssue) {
-                $("#eap_pin_" + reading.deviceSerialNumber + "").removeClass("eap_pin_good");
-            } else {
-                $("#eap_pin_" + reading.deviceSerialNumber + "").addClass("eap_pin_good");
-            }
-
-
         });
+
     });
 }
 
