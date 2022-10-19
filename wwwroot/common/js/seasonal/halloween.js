b1cdc7829f2c3b62154c53092565020c00a410a2
Mark Strendin <mark.strendin@lskysd.ca>
2022-10-19 12:37:29 -0600

    Start adding support for seasonal styles (#12)
    and add styles for halloween, day of the dead, and remembrance day.

---
+++ wwwroot/common/js/seasonal/halloween.js
@@ -0,0 +1,11 @@
+console.log("Loading Halloween stylesheets");
+
+// Add some ghosts to float around
+$('#dashboard-container-it1').append('<img src="../../img/seasonal/halloween/ghost2.png" id="halloween-ghost-1">');
+$('#dashboard-container-it1').append('<img src="../../img/seasonal/halloween/ghost.png" id="halloween-ghost-2">');
+$('#dashboard-container-it2').append('<img src="../../img/seasonal/halloween/ghost.png" id="halloween-ghost-3">');
+$('#dashboard-container-it2').append('<img src="../../img/seasonal/halloween/ghost2.png" id="halloween-ghost-4">');
+$('#dashboard-container-studsrv').append('<img src="../../img/seasonal/halloween/ghost.png" id="halloween-ghost-5">');
+$('#dashboard-container-studsrv').append('<img src="../../img/seasonal/halloween/ghost2.png" id="halloween-ghost-6">');
+
+// Add some webs in the corners
