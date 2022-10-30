de48ce78dbbbd3b71b69fff28464691ffc865878
Mark Strendin <mark.strendin@gmail.com>
2022-10-30 14:34:21 -0600

    Add some more Halloween tidbits

--- wwwroot/common/js/seasonal/halloween.js
+++ wwwroot/common/js/seasonal/halloween.js
@@ -7,5 +7,12 @@ $('#dashboard-container-it2').append('<img src="../../img/seasonal/halloween/gho
 $('#dashboard-container-it2').append('<img src="../../img/seasonal/halloween/ghost2.png" id="halloween-ghost-4">');
 $('#dashboard-container-studsrv').append('<img src="../../img/seasonal/halloween/ghost.png" id="halloween-ghost-5">');
 $('#dashboard-container-studsrv').append('<img src="../../img/seasonal/halloween/ghost2.png" id="halloween-ghost-6">');
+$('#dashboard-container-busgarage').append('<img src="../../img/seasonal/halloween/ghost2.png" id="halloween-ghost-7">');
 
 // Add some webs in the corners
+$('#dashboard-container-it1').append('<img src="../../img/seasonal/halloween/cobweb.png" id="halloween-cobweb-left">');
+$('#dashboard-container-it1').append('<img src="../../img/seasonal/halloween/cobweb_right.png" id="halloween-cobweb-right">');
+$('#dashboard-container-studsrv').append('<img src="../../img/seasonal/halloween/cobweb.png" id="halloween-cobweb-left">');
+$('#dashboard-container-studsrv').append('<img src="../../img/seasonal/halloween/cobweb_right.png" id="halloween-cobweb-right">');
+$('#dashboard-container-busgarage').append('<img src="../../img/seasonal/halloween/cobweb.png" id="halloween-cobweb-left">');
+$('#dashboard-container-busgarage').append('<img src="../../img/seasonal/halloween/cobweb_right.png" id="halloween-cobweb-right">');
