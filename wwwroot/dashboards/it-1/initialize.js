b327fdb57636c94db584fba8c894187bcd178e97
Mark Strendin <mark.strendin@gmail.com>
2023-02-18 20:27:57 -0600

    Remove mac minis from dashboard These are all now turned off, and will be decomissioned soon.

--- wwwroot/dashboards/it-1/initialize.js
+++ wwwroot/dashboards/it-1/initialize.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
     // Initialize school tiles 
     datatile_init("school_info_box_container", "DIV. OFFICE",1570,455, 87, 1, [{ id: 6, label: "BUSG" },{ id: 36, label: "SRVR"},{ id: 7, label: "LS"}], [1],[
         {"name": "DEPLOY-DO", "id": 32}, 
-        {"name": "Mac Mini", "id": 31}, 
         {"name": "SCCM", "id": 33}, 
         {"name": "Pi-BusGarage", "id": 44},
         {"name": "Pi-ServerRoom", "id": 34},
@@ -34,7 +33,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "BCS", 1350, 700, 364, 5, [{ id: 1}], [5],[
         {"name": "Alarm", "id": 116},
-        {"name": "Mac Mini", "id": 121},
         {"name": "IDRAC", "id": 118},
         {"name": "SCCM", "id": 119},
         {"name": "ESXI", "id": 117},
@@ -45,7 +43,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Bready",1140,145, 328, 6,  [{ id: 2}], [6],[
         {"name": "Alarm", "id": 122},
-        {"name": "Mac Mini", "id": 127},
         {"name": "IDRAC", "id": 124},
         {"name": "SCCM", "id": 125},
         {"name": "ESXI", "id": 123},
@@ -56,7 +53,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Cando",585,675, 604, 7, [ { id: 33, label: "SRVR"},{ id: 34, label: "ELEM"} ], [7],[
         {"name": "Alarm", "id": 128},
-        {"name": "Mac Mini", "id": 132},
         {"name": "IDRAC", "id": 130},
         {"name": "SCCM", "id": 131},
         {"name": "ESXI", "id": 129},
@@ -67,7 +63,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "CKCS",222,330, 621, 8, [{ id: 4, label: "SRVR"},{ id: 5, label: "STOR"}], [8],[
         {"name": "Alarm", "id": 134},
-        {"name": "Mac Mini", "id": 137},
         {"name": "IDRAC", "id": 135},
         {"name": "SCCM", "id": 136},
         {"name": "ESXI", "id": 133},
@@ -78,7 +73,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Connaught",1370,410, 246, 9, [{ id: 3}], [9],[
         {"name": "Alarm", "id": 138},
-        {"name": "Mac Mini", "id": 144},
         {"name": "IDRAC", "id": 140},
         {"name": "SCCM", "id": 141},
         {"name": "ESXI", "id": 139},
@@ -89,7 +83,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Hafford",801,403, 1935, 10, [{ id: 35}], [10],[
         {"name": "Alarm", "id": 146},
-        {"name": "Mac Mini", "id": 149},
         {"name": "IDRAC", "id": 147},
         {"name": "SCCM", "id": 148},
         {"name": "ESXI", "id": 145},
@@ -99,7 +92,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "HCES",898,197, 2059, 11, [{ id: 8}], [11],[
         {"name": "Alarm", "id": 150},
-        {"name": "Mac Mini", "id": 151},
         {"name": "IDRAC", "id": 153},
         {"name": "SCCM", "id": 154},
         {"name": "ESXI", "id": 152},
@@ -108,7 +100,6 @@ $(document).ready(function(){
     ]);
 
     datatile_init("school_info_box_container", "Heritage", 1558, 700, 1563, 12, [{ id: 9}], [12],[
-        {"name": "Mac Mini", "id": 158},
         {"name": "IDRAC", "id": 156},
         {"name": "SCCM", "id": 157},
         {"name": "ESXI", "id": 155},
@@ -120,7 +111,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Kerrobert",348,788, 1974, 13, [{ id: 10}], [13],[
         {"name": "Alarm", "id": 160},
-        {"name": "Mac Mini", "id": 164},
         {"name": "IDRAC", "id": 162},
         {"name": "SCCM", "id": 163},
         {"name": "ESXI", "id": 161},
@@ -130,7 +120,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Lawrence",1330,80, 522, 4, [{ id: 11}], [4],[
         {"name": "Alarm", "id": 165},
-        {"name": "Mac Mini", "id": 170},
         {"name": "IDRAC", "id": 167},
         {"name": "SCCM", "id": 168},
         {"name": "ESXI", "id": 166},
@@ -141,7 +130,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Leoville",630,30, 2987, 15, [{ id: 12}], [15],[
         {"name": "Alarm", "id": 171},
-        {"name": "Mac Mini", "id": 175},
         {"name": "IDRAC", "id": 173},
         {"name": "SCCM", "id": 174},
         {"name": "ESXI", "id": 172},
@@ -151,7 +139,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Luseland",50,690, 1873, 14, [{ id: 13}], [14],[
         {"name": "Alarm", "id": 176},
-        {"name": "Mac Mini", "id": 180},
         {"name": "IDRAC", "id": 178},
         {"name": "SCCM", "id": 179},
         {"name": "ESXI", "id": 177},
@@ -161,7 +148,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Macklin",16,512, 1779, 16, [{ id: 14, label: "SRVR"},{ id: 15, label: "HIGHS"}], [16],[
         {"name": "Alarm", "id": 181},
-        {"name": "Mac Mini", "id": 185},
         {"name": "IDRAC", "id": 183},
         {"name": "SCCM", "id": 184},
         {"name": "ESXI", "id": 182},
@@ -172,7 +158,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Maymont",715,530, 1519, 18, [{ id: 32}], [18],[
         {"name": "Alarm", "id": 186},
-        {"name": "Mac Mini", "id": 190},
         {"name": "IDRAC", "id": 188},
         {"name": "SCCM", "id": 189},
         {"name": "ESXI", "id": 187},
@@ -182,7 +167,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "McKitrick",1330,235, 1394, 19, [{ id: 16}], [19],[
         {"name": "Alarm", "id": 191},
-        {"name": "Mac Mini", "id": 196},
         {"name": "IDRAC", "id": 193},
         {"name": "SCCM", "id": 194},
         {"name": "ESXI", "id": 192},
@@ -193,7 +177,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "McLurg",478,345, 2111, 20, [{ id: 17, label: "ELEC"}], [20],[
         {"name": "Alarm", "id": 197},
-        {"name": "Mac Mini", "id": 201},
         {"name": "IDRAC", "id": 199},
         {"name": "SCCM", "id": 200},
         {"name": "ESXI", "id": 198},
@@ -203,7 +186,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "Medstead",500,188, 1362, 21, [{ id: 19}], [21],[
         {"name": "Alarm", "id": 202},
-        {"name": "Mac Mini", "id": 206},
         {"name": "IDRAC", "id": 204},
         {"name": "SCCM", "id": 205},
         {"name": "ESXI", "id": 203},
@@ -213,7 +195,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "NBCHS",1540,25, 1301, 17, [{ id: 37, label: "SRVR"},{ id: 21, label: "R206"},{ id: 22, label: "CHEM"},{ id: 23, label: "TELE"},{ id: 24, label: "MAINT"}], [17],[
         {"name": "Alarm", "id": 207},
-        {"name": "Mac Mini", "id": 212},
         {"name": "IDRAC", "id": 209},
         {"name": "SCCM", "id": 210},
         {"name": "ESXI", "id": 208},
@@ -228,7 +209,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "NCES",484,502, 2593, 22, [{ id: 20}], [22],[
         {"name": "Alarm", "id": 213},
-        {"name": "Mac Mini", "id": 217},        
         {"name": "IDRAC", "id": 215},
         {"name": "SCCM", "id": 216},
         {"name": "ESXI", "id": 214},
@@ -238,7 +218,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "SHS",700,197, 2655, 23, [{ id: 25, label: "SRVR"},{ id: 26, label: "LAB"}], [23],[
         {"name": "Alarm", "id": 253},
-        {"name": "Mac Mini", "id": 258},
         {"name": "IDRAC", "id": 255},
         {"name": "SCCM", "id": 256},
         {"name": "ESXI", "id": 254},
@@ -249,7 +228,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "St. Vital",1115,700, 2343, 26, [{ id: 27, label: "LIB"},{ id: 28, label: "R104"}], [26],[
         {"name": "Alarm", "id": 259},
-        {"name": "Mac Mini", "id": 264},
         {"name": "IDRAC", "id": 261},
         {"name": "SCCM", "id": 262},
         {"name": "ESXI", "id": 260},
@@ -261,7 +239,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "UCHS",265,508, 2531, 24, [{ id: 29, label: "SRVR"},{ id: 30, label: "BOIL"}], [24],[
         {"name": "Alarm", "id": 270},
-        {"name": "Mac Mini", "id": 269},
         {"name": "IDRAC", "id": 267},
         {"name": "SCCM", "id": 268},
         {"name": "ESXI", "id": 266},
@@ -272,7 +249,6 @@ $(document).ready(function(){
 
     datatile_init("school_info_box_container", "UPS", 265, 663, 2405, 25, [{ id: 31}], [25],[
         {"name": "Alarm", "id": 271},
-        {"name": "Mac Mini", "id": 275},
         {"name": "IDRAC", "id": 273},
         {"name": "SCCM", "id": 274},
         {"name": "ESXI", "id": 272},
