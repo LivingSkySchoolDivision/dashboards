0197f989b9bdf89cfab59f59468369946d965463
Mark Strendin <mark.strendin@gmail.com>
2025-07-03 15:13:25 -0600

    add phpipam server to dash-2

--- wwwroot/dashboards/it-2/initialize.js
+++ wwwroot/dashboards/it-2/initialize.js
@@ -82,6 +82,7 @@ $(document).ready(function(){
     librenms_simple_ping_tile("container-tilesensors", "ntp", 51);
     librenms_simple_ping_tile("container-tilesensors", "ovpn - monday", 68);
     librenms_simple_ping_tile("container-tilesensors", "papercut ", 49);
+    librenms_simple_ping_tile("container-tilesensors", "phpIPAM / IP ", 901);
     librenms_simple_ping_tile("container-tilesensors", "prismcentral", 83);
     librenms_simple_ping_tile("container-tilesensors", "pienvmon-virt-01", 445);
     librenms_simple_ping_tile("container-tilesensors", "public-vlan-dns", 335);
